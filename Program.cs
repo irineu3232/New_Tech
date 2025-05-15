c1752644f526faefdd11769003a871cca6c1849f
Paulo <irineu32323@gmail.com>
2025-05-15 17:28:11 -0300

    Passo 8 concluido

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddScoped<New_Tech.Repositorio.LoginRepositorio>();
 builder.Services.AddScoped<New_Tech.Repositorio.ProdutoRepositorio>();
 
